d8d431add8a42fc980827ef25e941fa3d8621ef0
Grasmus <91031013+Grasmus@users.noreply.github.com>
2021-11-17 22:22:52 +0200

    master

--- scripts/api_script.js
+++ scripts/api_script.js
@@ -1,46 +1,80 @@
-function main()
+const form =  document.getElementById('startingForm');
+form.addEventListener('submit', async function (event)
 {
-class Question
-{
-    constructor(info)
-    {
-        this.id = info["id"];
-        this.qustion = info["question"];
-        this.answers[0] = info["answer_a"];
-        this.answers[1] = info["answer_b"];
-        this.answers[2] = info["answer_c"];
-        this.answers[3] = info["answer_d"];
-        this.answers[4] = info["answer_e"];
-        this.answers[4] = info["answer_f"];
-        this.multiple_correct_answers = info["multiple_correct_answers"];
-        this.correct_answers[0] =  info["answer_a_correct"];
-        this.correct_answers[1] =  info["answer_b_correct"];
-        this.correct_answers[2] =  info["answer_c_correct"];
-        this.correct_answers[3] =  info["answer_d_correct"];
-        this.correct_answers[4] =  info["answer_e_correct"];
-        this.correct_answers[5] =  info["answer_f_correct"];
-        this.category = info["category"];
-        this.difficulty = info["difficulty"];
-    }
-    get_id()
-    {
-        return this.id;
-    }
-}
+    event.preventDefault();
+    API ='https://quizapi.io/api/v1/questions?apiKey=J35Z0YLOZ6U7Vf2THXXQe8FbRRvvq9FEOmHFgQO4';
 
-const API="http://quizapi.io/api/v1/questions?apiKey=J35Z0YLOZ6U7Vf2THXXQe8FbRRvvq9FEOmHFgQO4&limit=1";
-function getAPI(api)
+    difficultyList = document.getElementById('selectDifficulty');
+    difficultyIndex = difficultyList.selectedIndex;
+    difficulty = difficultyList[difficultyIndex].value;
+
+    categoryList = document.getElementById('selectCategory');
+    categoryIndex = categoryList.getSelectedIndex;
+    category = categoryList[categoryIndex];
+
+    limitElement = form.elements;
+    limit = limitElement['numOfQuestions'].value;
+   // alert(limit);
+
+    if(limit)
+    {
+        API += '&limit=' + limit.toString();
+    }
+    if(difficulty)
+    {
+        API += '&difficulty=' + difficulty.toString();
+    }
+    if(category)
+    {
+        API += '&category=' + category.toString();
+    }
+ //   alert(API);
+   
+
+    let response = await fetch(API);
+    let questions = await response.json();
+
+    startQuiz(questions);
+ //   alert(questions.length);
+   // alert(questions[0].difficulty);
+})
+
+function startQuiz(questions)
 {
-    const xmlHTTP= fetch(API);
-    return xmlHTTP;
-}
-const APIstring = getAPI(API);
-alert(APIstring);
-//const APIJson = JSON.parse(APIstring);
-function create_quiz()
-{
-    const question = new Question(APIJson);
-    alert(question.get_id());
-}
-//create_quiz();
+   i = 0;
+ //  alert(questions.length);
+  //  alert(questions[0].answers.answer_a);
+    form.parentNode.removeChild(form);
+    body = document.body; 
+    str = '<p>'+ questions[i].question + '</p> <form>'
+      +'<lable for="first_answer">'+ questions[i].answers.answer_a +'</lable>' 
+      +'<input type="radio" id="first_answer"><br>';
+      if(questions[i].answers.answer_b) 
+      {
+          str += '<lable for="second_answer">'+ questions[i].answers.answer_b +'</lable>' 
+          + '<input type="radio" id="second_answer"><br>';
+      }
+      if(questions[i].answers.answer_c) 
+      {
+          str += '<lable for="third_answer">'+ questions[i].answers.answer_c +'</lable>'
+          +'<input type="radio" id="third_answer"><br>';
+      }
+      if(questions[i].answers.answer_d) 
+      {
+          str += '<lable for="fourth_answer">'+ questions[i].answers.answer_d +'</lable>'
+          +'<input type="radio" id="fourth_answer"><br>';
+      }
+      if(questions[i].answers.answer_e) 
+      {
+          str += '<lable for="fifth_answer">'+ questions[i].answers.answer_e +'</lable>'
+          +'<input type="radio" id="fifth_answer"><br>';
+      }
+      if(questions[i].answers.answer_f) 
+      {
+          str += '<lable for="sixth_answer">'+ questions[i].answers.answer_f +'</lable>'
+          +'<input type="radio" id="sixth_answer"><br>';
+      }
+      str += '</form>';
+      body.innerHTML  = str;
+
 }
